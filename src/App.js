b4ad05453fdca347391a9680bbbc86c59af23314
maruf3535 <marufrafsan677@gmail.com>
2022-06-01 01:40:52 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+let name = "maruf35";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React With Rafsan
-        </a>
-      </header>
-    </div>
+    <>
+      <h3 id="heading">Hello {name}</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, in reprehenderit! Modi veniam soluta fugiat exercitationem <b>provident</b>, eos praesentium aliquid dignissimos! Dignissimos modi, eius quibusdam rerum ab laborum dicta. Deleniti.</p>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React With CodeWithHarry
+          </a>
+        </header>
+      </div>
+    </>
   );
 }
 
