3aba8d03527e917ef3bc75b68260ed29d2eed3cc
maruf3535 <marufrafsan677@gmail.com>
2022-06-02 00:32:58 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
-let name = "maruf35";
 function App() {
   return (
     <>
-      <h3 id="heading">Hello {name}</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, in reprehenderit! Modi veniam soluta fugiat exercitationem <b>provident</b>, eos praesentium aliquid dignissimos! Dignissimos modi, eius quibusdam rerum ab laborum dicta. Deleniti.</p>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React With CodeWithHarry
-          </a>
-        </header>
-      </div>
+      {/* <Navbar title="TextUtils" about_text="About TextUtils" home_link="https://www.google.com"/> */}
+      {/* <Navbar /> */}
+      <Navbar title="TitlePassed"/>
     </>
   );
 }
---
+++ tut-note/module1.js
@@ -0,0 +1,6 @@
+const name = 'Rafsan';
+const type = 'Bhai';
+const age = 32;
+
+// export default type;
+exports.values = {name, type, age};
