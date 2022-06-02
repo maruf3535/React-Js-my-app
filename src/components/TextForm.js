4ce22e1a4fc73b1d002d6aec31663b9c2fcef5ba
maruf3535 <marufrafsan677@gmail.com>
2022-06-03 01:43:32 +0600

    done

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -8,16 +8,37 @@ export default function TextForm() {
         setText(newText);
     }
 
-    const handleOnChange = (event)=>{
+    const handleOnChange = (event) => {
         setText(event.target.value);
     }
+
+    // Counting the exact words number.
+    let words = text.split(' ');
+    let words_len = words.length;
+    for (let i in words){
+        if (words[i] == ''){
+            words_len -= 1;
+        }
+    }
+
     return (
-        <div>
-            <div className="mb-3">
-                <label htmlFor="text" className="form-label">Example textarea</label>
-                <textarea className="form-control" id="text" rows="6" value={text} onChange={handleOnChange}></textarea>
+        <>
+            <div>
+                <div className="mb-3">
+                    <label htmlFor="text" className="form-label">Example textarea</label>
+                    <textarea className="form-control" id="text" rows="6" value={text} onChange={handleOnChange}></textarea>
+                </div>
+                <button className="btn btn-primary" onClick={handleToUppercase}>Change to Uppercase</button>
             </div>
-            <button className="btn btn-primary" onClick={handleToUppercase}>Change to Uppercase</button>
-        </div>
+            <div className="container my-3">
+                <h4>Your Text Summmary</h4>
+                <p className="fs-6">
+                    <b> {words_len} </b>
+                    Words and
+                    <b> {text.length} </b>
+                    Characters
+                </p>
+            </div>
+        </>
     )
 }
